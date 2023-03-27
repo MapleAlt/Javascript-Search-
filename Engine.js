db3b7e0bceb40c6a9864059e9e1212b099ed41e5
MapleAlt <128828957+MapleAlt@users.noreply.github.com>
2023-03-27 10:08:00 -0400

    Update Engine.js

--- Engine.js
+++ Engine.js
@@ -1,15 +1,3 @@
-// Dont include comments when copy and pasting
-// This engine was made by MapleVR
-
-// Console command
-alert("If the pop up is blocked click on the blocked link!");
-let Search = prompt("Please enter the URL of the wbesite you want to go! If the pop up is blocked, click on the block link. If you want to add a space dont hit the space bar hit the + key instead.");
-if (Search != null) 
-{  
-    let newTab = window.open("/search?q=" + Search + "&safe=active&ssui=on");
-    newTab.location.href = url;
-}
-
 // Bookmarklet
 javascript: alert("If the pop up is blocked click on the blocked link!");
 let Search = prompt("Please enter the URL of the wbesite you want to go! If the pop up is blocked, click on the block link. If you want to add a space dont hit the space bar hit the + key instead.");
