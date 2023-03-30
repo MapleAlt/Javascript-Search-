cfefe942cf7b3e7445a25943bdd2f8ad3ae2e619
MapleAlt <128828957+MapleAlt@users.noreply.github.com>
2023-03-30 09:39:48 -0400

    Update Beta.js

--- Beta.js
+++ Beta.js
@@ -1,10 +1,1 @@
-// Dont copy the comments
-// Search up somthing random on google then activate the bookmarklet
-// Bookmarklet
-javascript: alert("If the site gets pop up blocked click the blocked link and it will take you to the site");
-let Search = prompt("Welcome to Javascript Search. Please enter a URL to search down below then click ok. If you want to add a space use the + key instead of the space bar! Press cancel to stop!");
-if (Search != null) 
-{  
-    let newTab = window.open("https://google.com");("/search?q=" + Search + "&safe=active&ssui=on");
-    newTab.location.href = url;
-}
+// Nothing here. Please use the release build!
