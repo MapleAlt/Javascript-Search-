236ef707e464f98e96866d15d8fdb14963a29de7
MapleAlt <128828957+MapleAlt@users.noreply.github.com>
2023-03-30 09:43:17 -0400

    Update Engine.js

--- Engine.js
+++ Engine.js
@@ -1,2 +1,12 @@
-// Please check status
-// If you want to try it out please check the betas
+// Do not copy comments
+// Regular Code
+let Search = prompt("Hello, and thanks for using Javascript Search. Please enter what you want to search below. If the pop up is blocked just click on the blocked link! Made by Maple and Maskedwrrg on Github!");
+if (Search != null)
+{
+    query = Search;
+    url ='http://www.google.com/search?q=' + query;
+    window.open(url,'_blank');
+    alert("Search has been completed! Made by Maple and Maskedwrrg on Github!");
+}
+// Bookmarklet
+javascript:javascript: let Search = prompt("Hello, and thanks for using Javascript Search. Please enter what you want to search below. If the pop up is blocked just click on the blocked link! Made by Maple and Maskedwrrg on Github!");if (Search != null){    query = Search;    url ='http://www.google.com/search?q=%27 + query;    window.open(url,%27_blank%27);    alert("Search has been completed! Made by Maple and Maskedwrrg on Github!");}
